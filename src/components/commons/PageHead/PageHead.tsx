201fd5a72fdd3a7496a822a2be365aed185e106b
rizky.ariananda <rizkyari4121@gmail.com>
2025-05-30 11:04:08 +0700

    feat: activation account from email

--- src/components/commons/PageHead/PageHead.tsx
+++ src/components/commons/PageHead/PageHead.tsx
@@ -12,6 +12,7 @@ const PageHead = (props: PropTypes) => {
             <title>{title}</title>
             <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="icon" href="/images/general/logo.svg" type="image/x-icon"/>
         </Head>
     )
 };
--- src/components/layouts/AuthLayout/AuthLayout.tsx
+++ src/components/layouts/AuthLayout/AuthLayout.tsx
@@ -1,5 +1,5 @@
 import PageHead from "@/components/commons/PageHead"
-import { ReactNode } from "react";
+import { Fragment, ReactNode } from "react";
 
 interface PropTypes {
     title?: string;
@@ -11,12 +11,12 @@ const AuthLayout = (props: PropTypes) => {
     const {title, children} = props;
 
     return (
-        <>
+        <Fragment>
             <PageHead title={title}/>
             <section className="max-w-screen-3xl 3xl:container p-6">
                 {children}
             </section>
-        </>
+        </Fragment>
     )
 }
 
