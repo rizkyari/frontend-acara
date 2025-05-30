b294d32682580eb736e3f5b28a9effbe2f5b32ad
rizky.ariananda <rizkyari4121@gmail.com>
2025-05-30 08:29:05 +0700

    feat:implement register function

---
+++ src/components/views/Register/Register.tsx
@@ -0,0 +1,89 @@
+import { Button, Card, CardBody, Input, Spinner } from "@nextui-org/react";
+import Image from "next/image";
+import Link from "next/link";
+import useRegister from "./useRegister";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa6";
+import { Controller } from "react-hook-form";
+import { cn } from "@/utils/cn";
+
+const Register = () => {
+
+    const {visiblePassword, handleVisiblePassword, control, handleSubmit, handleRegister, isPendingRegister, errors} = useRegister();
+
+    return (
+        <div className="flex w-full items-center justify-center flex-col lg:flex-row gap-10 lg:gap-20">
+            <div className="flex w-full lg:w-1/3 flex-col items-center justify-center gap-10">
+                <Image src="/images/general/logo.svg" alt="logo" width={180} height={180}/>
+                <Image src="/images/illustrations/login.svg" className="w-2/3 lg:w-full" alt="login" width={1024} height={1024}/>
+            </div>
+            <Card className="p-8">
+                <CardBody>
+                    <h2 className="text-xl font-bold text-danger-500">Create Account</h2>
+                    <p className="text-small mb-4">
+                        Have an account?&nbsp;
+                        <Link href="/auth/login" className="font-semibold text-danger-400">Login here</Link>    
+                    </p>
+                    { errors.root && (
+                        <p className="mb-2 font-medium text-danger">{errors?.root?.message}</p>
+                    )}
+                    <form className={cn("flex w-80 flex-col", Object.keys(errors).length > 0 ? "gap-2" : "gap-4")} onSubmit={handleSubmit(handleRegister)}>
+                        <Controller name="fullname" control={control} render={({field})=>(
+                            <Input {...field} type="text" label="Fullname" variant="bordered" autoComplete="off" isInvalid={errors.fullname !== undefined} errorMessage={errors.fullname?.message}/>
+                        )}/>
+                        <Controller name="username" control={control} render={({field}) => (
+                            <Input {...field} type="text" label="Username" variant="bordered" autoComplete="off" isInvalid={errors.username !== undefined} errorMessage={errors.username?.message}/>
+                        )}/>
+                        <Controller name="email" control={control} render={({field}) => (
+                            <Input {...field} type="email" label="Email" variant="bordered" autoComplete="off" isInvalid={errors.email !== undefined} errorMessage={errors.email?.message}/>
+                        )}/>
+                        <Controller name="password" control={control} render={({field}) => (
+                            <Input 
+                            {...field}
+                            type={visiblePassword.password ? 'text' : 'password'} 
+                            label="Password" 
+                            variant="bordered" 
+                            autoComplete="off"
+                            endContent={
+                                <button 
+                                className="focus:outline-none"
+                                type="button"
+                                onClick={() => handleVisiblePassword("password")}
+                                >
+                                    {visiblePassword.password ? <FaEye className="text-xl text-default-400 pointer-events-none"/> : <FaEyeSlash className="text-xl text-default-400 pointer-events-none"/>}
+                                </button>
+                            }
+                            isInvalid={errors.password !== undefined} 
+                            errorMessage={errors.password?.message}
+                            />
+                        )}/>
+                        <Controller name="confirmPassword" control={control} render={({field})=>(
+                            <Input 
+                            {...field}
+                            type={visiblePassword.confirmPassword ? 'text' : 'password'} 
+                            label="Password Confirmation" 
+                            variant="bordered" 
+                            autoComplete="off"
+                            endContent={
+                                <button 
+                                className="focus:outline-none"
+                                type="button"
+                                onClick={() => handleVisiblePassword("confirmPassword")}
+                                >
+                                    {visiblePassword.confirmPassword ? <FaEye className="text-xl text-default-400 pointer-events-none"/> : <FaEyeSlash className="text-xl text-default-400 pointer-events-none"/>}
+                                </button>
+                            }
+                            isInvalid={errors.confirmPassword !== undefined} 
+                            errorMessage={errors.confirmPassword?.message}
+                            />
+                        )}/>
+                        
+                        <Button color="danger" size="lg" type="submit">{isPendingRegister ? <Spinner color="white" size="sm"/> : "Register"}</Button>
+                    </form>
+                </CardBody>
+            </Card>
+        </div>
+    )
+}
+
+export default Register;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,35 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import {NextUIProvider} from "@nextui-org/react"
+import { Inter } from "next/font/google";
+import { cn } from "@/utils/cn";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+})
+
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    }
+  }
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>
+        <main className={cn(
+          inter.className,
+          "flex min-h-screen min-w-full flex-col items-center justify-center gap-10 py-10 lg:py"
+        )}>
+          <Component {...pageProps} />
+        </main>
+      </NextUIProvider>
+    </QueryClientProvider>
   );
 }
